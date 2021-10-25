b0e8cd8799dfe8494c8fc16c2cd444c2b66dc09c
Jason Berger <JBerger@up-rev.com>
2021-10-24 23:38:58 -0400

    Step 1: Removing old code

--- main/gatts_table_creat_demo.h
+++ main/gatts_table_creat_demo.h
@@ -11,20 +11,3 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-/* Attributes State Machine */
-enum
-{
-    IDX_SVC,
-    IDX_CHAR_A,
-    IDX_CHAR_VAL_A,
-    IDX_CHAR_CFG_A,
-
-    IDX_CHAR_B,
-    IDX_CHAR_VAL_B,
-
-    IDX_CHAR_C,
-    IDX_CHAR_VAL_C,
-
-    HRS_IDX_NB,
-};
